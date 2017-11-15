59b0b9b6f9b6305fe2b743403237cdfe9e1beb59
Chris Peterson <cpeterso@cpeterso.com>
2017-11-15 12:00:33 -0800

    Increase startup delay to three seconds

--- src/background.js
+++ src/background.js
@@ -20,5 +20,5 @@ browser.windows.onRemoved.addListener(() => {
 
 // We don't have a chance to clear Flash cookies during browser shutdown, so
 // we clear the previous browser session's Flash cookies during browser startup.
-// Wait a second before clearing so we don't jank browser startup.
-window.setTimeout(removePluginData, 1000);
+// Wait a couple seconds before clearing so we don't jank browser startup.
+window.setTimeout(removePluginData, 3000);
