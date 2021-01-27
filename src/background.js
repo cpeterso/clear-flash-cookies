612cc6a3a67e2232fe73fc76d0cd7d8d09b7fd65
Chris Peterson <cpeterson@mozilla.com>
2021-01-26 22:36:44 -0800

    Automatically uninstall extension because it's no longer relevant now that Flash is EOL

--- src/background.js
+++ src/background.js
@@ -1,35 +1,3 @@
 "use strict";
 
-function removePluginData() {
-    const start = performance.now();
-
-    function logDuration() {
-        const end = performance.now();
-        const duration = Math.ceil(end - start);
-        console.log(`clear-flash-cookies: removePluginData took ${duration} ms`);
-    }
-
-    browser.browsingData.removePluginData({}).then(() => {
-        logDuration();
-    }).catch(error => {
-        logDuration();
-        console.error(`clear-flash-cookies: error! ${error}`);
-    });
-}
-
-// Remove Flash cookies when all browser windows are closed. This code does
-// not run on browser exit.
-browser.windows.onRemoved.addListener(() => {
-    browser.windows.getAll().then(windows => {
-        if (windows.length === 0) {
-            removePluginData();
-        }
-    }).catch(error => {
-        console.error(`clear-flash-cookies: error! ${error}`);
-    });
-});
-
-// We don't have a chance to clear Flash cookies during browser shutdown, so
-// we clear the previous browser session's Flash cookies during browser startup.
-// Wait a couple seconds before clearing so we don't jank browser startup.
-window.setTimeout(removePluginData, 3000);
+browser.management.uninstallSelf();
